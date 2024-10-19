85216aa9122acce135b0f7a5671eb33babd94a0d
Arun Kumar <kumar.arun.ynr@gmail.com>
2024-10-19 20:39:25 +0530

    Initial Commit

---
+++ OnboardingFirebase/OnboardingFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingFirebaseApp.swift
+//  OnboardingFirebase
+//
+//  Created by Arun on 19/10/24.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
